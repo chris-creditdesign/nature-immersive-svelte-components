300582983a9fdc84791256a5b2b3c2243220c335
Chris Ryan <creditdesign@gmail.com>
2020-04-13 11:35:42 +0100

    Adds LayoutFrame component

---
+++ src/components/LayoutFrame/view.default.svelte
@@ -0,0 +1,11 @@
+<script>
+  import LayoutFrame from "./index.svelte";
+
+  export let className = "";
+  export let frameRatioHeight;
+  export let frameRatioWidth;
+</script>
+
+<LayoutFrame {className} {frameRatioHeight} {frameRatioWidth}>
+  <img src="img/image.jpg" alt="test image" />
+</LayoutFrame>
---
+++ src/components/LayoutFrame/view.place-holder.svelte
@@ -0,0 +1,22 @@
+<script>
+  import LayoutFrame from "./index.svelte";
+
+  export let className = "";
+  export let frameRatioHeight;
+  export let frameRatioWidth;
+</script>
+
+<style>
+  .place-holder {
+    background-color: #ececec;
+    font-family: helvetica;
+    text-transform: uppercase;
+    font-weight: bold;
+  }
+</style>
+
+<LayoutFrame {className} {frameRatioHeight} {frameRatioWidth}>
+  <div class="place-holder">
+    <p>image pending</p>
+  </div>
+</LayoutFrame>
