6bb222bc4962cef9ce311e9f291362570e7eca4a
Chris Ryan <creditdesign@gmail.com>
2020-05-11 20:16:04 +0100

    Removes onMount from ImageSlider component

--- src/components/ImageSlider/index.svelte
+++ src/components/ImageSlider/index.svelte
@@ -1,21 +1,16 @@
 <script>
-  import { onMount } from "svelte";
   import Interactive from "./interactive.svelte";
   import Static from "./static.svelte";
 
+  export let mounted = false;
   export let className = "";
   export let ratio;
   export let imageData;
   export let message;
 
-  let mounted = false;
   let componentMap = new Map();
   componentMap.set(true, Interactive);
   componentMap.set(false, Static);
-
-  onMount(() => {
-    mounted = true;
-  });
 </script>
 
 <svelte:component
--- src/components/ImageSlider/view.default.svelte
+++ src/components/ImageSlider/view.default.svelte
@@ -7,10 +7,16 @@
   export let ratio;
   export let imageData;
   export let message;
+
+  let mounted = false;
+
+  onMount(() => {
+    mounted = true;
+  });
 </script>
 
 <Center>
   <Box boxSpace="var(--s3)">
-    <ImageSlider {className} {ratio} {imageData} {message} />
+    <ImageSlider {className} {ratio} {imageData} {message} bind:mounted />
   </Box>
 </Center>
