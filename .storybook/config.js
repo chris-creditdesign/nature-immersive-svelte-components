8b164c0be2f1f5d2c8433a73577a5b1c9d5a4ef9
Chris Ryan <creditdesign@gmail.com>
2020-06-16 18:52:34 +0100

    Splits css custom props into separate file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,9 @@
 import { configure } from '@storybook/svelte';
 
 import "../static/css/harding.css"
-import "../static/css/index.css"
+import "../static/css/custom-props.css"
 import "../static/css/light-theme.css"
+import "../static/css/index.css"
 
 // automatically import all files ending in *.stories.js
 configure(require.context('../src/components', true, /\.stories\.js$/), module);
