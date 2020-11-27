c52a4a7d4b60182d3db35972f3d0591bc1ae1346
Chris Ryan <creditdesign@gmail.com>
2020-11-27 07:09:50 +0000

    Adds check for document and window before loading youtube api script

--- src/components/YouTube/index.svelte
+++ src/components/YouTube/index.svelte
@@ -3,14 +3,18 @@
 
   // https://developers.google.com/youtube/iframe_api_reference
   // This code loads the IFrame Player API code asynchronously.
-  let tag = document.createElement("script");
-  tag.src = "https://www.youtube.com/iframe_api";
-  let firstScriptTag = document.getElementsByTagName("script")[0];
-  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  if (document !== undefined) {
+    let tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    let firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  }
 
-  window.onYouTubeIframeAPIReady = function () {
-    youTubeIframeAPIReady.set(true);
-  };
+  if (window !== undefined) {
+    window.onYouTubeIframeAPIReady = function () {
+      youTubeIframeAPIReady.set(true);
+    };
+  }
 </script>
 
 <script>
