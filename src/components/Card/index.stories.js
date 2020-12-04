6e18c58a74b3047332ad03e499f20f01592cbf32
Chris Ryan <creditdesign@gmail.com>
2020-12-04 19:36:45 +0000

    Updates Menu component to justify-content space between

--- src/components/Card/index.stories.js
+++ src/components/Card/index.stories.js
@@ -1,18 +1,19 @@
+import { withKnobs, object, text } from "@storybook/addon-knobs";
 import ViewDefault from "./index.svelte";
 import Card from "./view.constrained.svelte";
 import cardData from "../preview-content/card-data.js";
 import docs from "./docs.mdx";
 
-const cardDataNoImage = { ...cardData, srcURL: undefined };
-const cardDataNoAuthor = {
-  ...cardData,
-  subHead: undefined,
-};
-const cardDataNoAuthorNoJournal = {
-  ...cardData,
-  subHead: undefined,
-  eyebrow: undefined,
-};
+// const cardDataNoImage = { ...cardData, srcURL: undefined };
+// const cardDataNoAuthor = {
+//   ...cardData,
+//   subHead: undefined,
+// };
+// const cardDataNoAuthorNoJournal = {
+//   ...cardData,
+//   subHead: undefined,
+//   eyebrow: undefined,
+// };
 
 export default {
   title: "Layout components/Card/Card",
@@ -25,101 +26,102 @@ export default {
 
 export const Default = () => ({
   Component: ViewDefault,
+  decorators: [withKnobs],
   props: {
-    className: "test",
-    cardData,
+    headerLevel: text("headerLevel", "h2"),
+    cardData: object("cardData", { ...cardData }),
   },
 });
 
-export const ConstrainedWidth = () => ({
-  Component: Card,
-  props: {
-    className: "test",
-    id: "test",
-    headerLevel: "h2" /* default */,
-    stackSpace: "var(--s-1)" /* default */,
-    cardData: {
-      subHead: "Author Name <i>et al</i>.",
-      eyebrow: "Journal Name",
-      altText: "This is the alt text",
-      caption: "This is the caption",
-      srcURL: "img/image.jpg",
-      captionSpace: "" /* What is this? */,
-      footnote: "Footnote",
-      headline: "Headline",
-      href: "https://www.nature.com",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, corrupti aut. Natus, ducimus. Qui beatae, id quasi neque veniam sequi doloremque, officia voluptatibus expedita optio repudiandae quos ad! Aut, rerum. <a href='https://www.nature.com'>Read more.</a>",
-    },
-  },
-});
+// export const ConstrainedWidth = () => ({
+//   Component: Card,
+//   props: {
+//     className: "test",
+//     id: "test",
+//     headerLevel: "h2" /* default */,
+//     stackSpace: "var(--s-1)" /* default */,
+//     cardData: {
+//       subHead: "Author Name <i>et al</i>.",
+//       eyebrow: "Journal Name",
+//       altText: "This is the alt text",
+//       caption: "This is the caption",
+//       srcURL: "img/image.jpg",
+//       captionSpace: "" /* What is this? */,
+//       footnote: "Footnote",
+//       headline: "Headline",
+//       href: "https://www.nature.com",
+//       text:
+//         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, corrupti aut. Natus, ducimus. Qui beatae, id quasi neque veniam sequi doloremque, officia voluptatibus expedita optio repudiandae quos ad! Aut, rerum. <a href='https://www.nature.com'>Read more.</a>",
+//     },
+//   },
+// });
 
-export const NoLink = () => ({
-  Component: Card,
-  props: {
-    className: "test",
-    id: "test",
-    headerLevel: "h2" /* default */,
-    stackSpace: "var(--s-1)" /* default */,
-    cardData: {
-      subHead: "Author Name <i>et al</i>.",
-      eyebrow: "Journal Name",
-      altText: "This is the alt text",
-      caption: "This is the caption",
-      srcURL: "img/image.jpg",
-      captionSpace: "" /* What is this? */,
-      footnote: "Footnote",
-      headline: "Headline",
-      href: undefined,
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, corrupti aut. Natus, ducimus. Qui beatae, id quasi neque veniam sequi doloremque, officia voluptatibus expedita optio repudiandae quos ad! Aut, rerum. <a href='https://www.nature.com'>Read more.</a>",
-    },
-  },
-});
+// export const NoLink = () => ({
+//   Component: Card,
+//   props: {
+//     className: "test",
+//     id: "test",
+//     headerLevel: "h2" /* default */,
+//     stackSpace: "var(--s-1)" /* default */,
+//     cardData: {
+//       subHead: "Author Name <i>et al</i>.",
+//       eyebrow: "Journal Name",
+//       altText: "This is the alt text",
+//       caption: "This is the caption",
+//       srcURL: "img/image.jpg",
+//       captionSpace: "" /* What is this? */,
+//       footnote: "Footnote",
+//       headline: "Headline",
+//       href: undefined,
+//       text:
+//         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, corrupti aut. Natus, ducimus. Qui beatae, id quasi neque veniam sequi doloremque, officia voluptatibus expedita optio repudiandae quos ad! Aut, rerum. <a href='https://www.nature.com'>Read more.</a>",
+//     },
+//   },
+// });
 
-export const HeaderLevelThree = () => ({
-  Component: Card,
-  props: {
-    className: "test",
-    id: "test",
-    headerLevel: "h3",
-    stackSpace: "var(--s-1)" /* default */,
-    cardData: {
-      subHead: "Author Name <i>et al</i>.",
-      eyebrow: "Journal Name",
-      altText: "This is the alt text",
-      caption: "This is the caption",
-      srcURL: "img/image.jpg",
-      captionSpace: "" /* What is this? */,
-      footnote: "Footnote",
-      headline: "Headline",
-      href: "https://www.nature.com",
-      text:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, corrupti aut. Natus, ducimus. Qui beatae, id quasi neque veniam sequi doloremque, officia voluptatibus expedita optio repudiandae quos ad! Aut, rerum. <a href='https://www.nature.com'>Read more.</a>",
-    },
-  },
-});
+// export const HeaderLevelThree = () => ({
+//   Component: Card,
+//   props: {
+//     className: "test",
+//     id: "test",
+//     headerLevel: "h3",
+//     stackSpace: "var(--s-1)" /* default */,
+//     cardData: {
+//       subHead: "Author Name <i>et al</i>.",
+//       eyebrow: "Journal Name",
+//       altText: "This is the alt text",
+//       caption: "This is the caption",
+//       srcURL: "img/image.jpg",
+//       captionSpace: "" /* What is this? */,
+//       footnote: "Footnote",
+//       headline: "Headline",
+//       href: "https://www.nature.com",
+//       text:
+//         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, corrupti aut. Natus, ducimus. Qui beatae, id quasi neque veniam sequi doloremque, officia voluptatibus expedita optio repudiandae quos ad! Aut, rerum. <a href='https://www.nature.com'>Read more.</a>",
+//     },
+//   },
+// });
 
-export const NoImage = () => ({
-  Component: Card,
-  props: {
-    className: "test",
-    cardData: cardDataNoImage,
-  },
-});
+// export const NoImage = () => ({
+//   Component: Card,
+//   props: {
+//     className: "test",
+//     cardData: cardDataNoImage,
+//   },
+// });
 
-export const NoAuthor = () => ({
-  Component: Card,
-  props: {
-    className: "test",
-    cardData: cardDataNoAuthor,
-  },
-});
+// export const NoAuthor = () => ({
+//   Component: Card,
+//   props: {
+//     className: "test",
+//     cardData: cardDataNoAuthor,
+//   },
+// });
 
-export const NoJournalNoJournal = () => ({
-  Component: Card,
-  props: {
-    className: "test",
-    cardData: cardDataNoAuthorNoJournal,
-  },
-});
+// export const NoJournalNoJournal = () => ({
+//   Component: Card,
+//   props: {
+//     className: "test",
+//     cardData: cardDataNoAuthorNoJournal,
+//   },
+// });
--- src/components/Menu/components/MenuList/index.svelte
+++ src/components/Menu/components/MenuList/index.svelte
@@ -1,20 +1,61 @@
 <script>
-  import { SwitcherList } from "creditdesign-svelte-components";
-
   export let menuLinks;
 </script>
 
-<SwitcherList>
-  {#each menuLinks as { text, href }}
-    <li>
-      <a
-        {href}
-        data-event-category="menu"
-        data-event-action="click"
-        data-event-label="{text}"
-      >
-        {text}
-      </a>
-    </li>
-  {/each}
-</SwitcherList>
+<style>
+  .switcher--list {
+    --switcher-space: var(--s-1);
+    --switcher-min-width: var(--measure);
+    --modifier: calc(
+      var(--switcher-min-width) - (100% - var(--switcher-space))
+    );
+  }
+
+  .switcher--list__inner {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: none;
+    padding: 0;
+    margin: calc((var(--switcher-space) / 2) * -1);
+    list-style: none;
+  }
+
+  :global(.switcher--list__inner > *) {
+    flex-basis: calc(var(--modifier) * 999);
+
+    /* flex-grow: 1; */
+    margin: calc(var(--switcher-space) / 2);
+  }
+
+  :global(.switcher--list__inner > li) {
+    /* Remove default padding */
+    padding: 0;
+  }
+
+  /* If there are more than 7 items, stack them automatically */
+  :global(.switcher--list__inner > :nth-last-child(n + 8)) {
+    flex-basis: 100%;
+  }
+
+  :global(.switcher--list__inner > :nth-last-child(n + 8) ~ *) {
+    flex-basis: 100%;
+  }
+</style>
+
+<div class="switcher--list">
+  <ul class="switcher--list__inner">
+    {#each menuLinks as { text, href }}
+      <li>
+        <a
+          {href}
+          data-event-category="menu"
+          data-event-action="click"
+          data-event-label="{text}"
+        >
+          {text}
+        </a>
+      </li>
+    {/each}
+  </ul>
+</div>
