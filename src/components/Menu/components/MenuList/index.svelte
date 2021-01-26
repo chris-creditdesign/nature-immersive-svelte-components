50bca51bc63deafe9f978f23e313013ef4872de7
Chris Ryan <creditdesign@gmail.com>
2021-01-26 14:18:32 +0000

    Menu: Updates menu list so that elems are aranged with space-between
    - 0.3.10

--- src/components/Menu/components/MenuList/index.svelte
+++ src/components/Menu/components/MenuList/index.svelte
@@ -1,7 +1,6 @@
 <script>
   import { slide } from "svelte/transition";
   import { quadIn, quadOut } from "svelte/easing";
-  import { SwitcherList } from "creditdesign-svelte-components";
 
   export let menuLinks;
   export let lastMenuLink;
@@ -11,17 +10,19 @@
 </script>
 
 <style>
-  a {
-    text-decoration: none;
-  }
-
   nav {
+    --switcher-min-width: var(--measure);
+    --switcher-space: var(--s-1);
+    --modifier: calc(
+      var(--switcher-min-width) - (100% - var(--switcher-space))
+    );
+
     position: absolute;
     top: calc(1.6em + var(--s-1) + var(--s-1));
     left: 0;
     width: 100vw;
     max-width: none;
-    padding: var(--s-1);
+    padding: var(--s-2);
     border-top: 1px solid var(--text-color-invert);
   }
 
@@ -33,6 +34,36 @@
       animation-delay: 0ms !important;
     }
   }
+
+  .menu-switcher {
+    overflow: hidden;
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    max-width: none;
+    padding: 0;
+    margin: calc((var(--switcher-space) / 2) * -1);
+    list-style: none;
+  }
+
+  li {
+    flex-basis: calc(var(--modifier) * 999);
+    min-width: fit-content;
+    margin: calc(var(--switcher-space) / 2);
+  }
+
+  /* Used for the case where there are more than 8 list items
+	so that the will be stacked automatically. Appiled with JS. */
+  .flex-basis\:100 {
+    flex-basis: 100%;
+  }
+
+  .text-decoration\:none {
+    text-decoration: none;
+  }
 </style>
 
 <nav
@@ -41,36 +72,41 @@
   in:slide="{{ duration: 250, easing: quadOut }}"
   out:slide="{{ duration: 250, easing: quadIn }}"
 >
-  <SwitcherList>
-    {#each menuLinks as { text, href }, i}
-      {#if i !== menuLinks.length - 1}
-        <li>
-          <a
-            {href}
-            on:focus
-            on:blur
-            data-event-category="menu"
-            data-event-action="click"
-            data-event-label="{text}"
-          >
-            {@html text}
-          </a>
-        </li>
-      {:else}
-        <li>
-          <a
-            {href}
-            on:focus
-            on:blur
-            bind:this="{lastMenuLink}"
-            data-event-category="menu"
-            data-event-action="click"
-            data-event-label="{text}"
-          >
-            {@html text}
-          </a>
-        </li>
-      {/if}
-    {/each}
-  </SwitcherList>
+  <div class="menu-switcher">
+
+    <ul>
+      {#each menuLinks as { text, href }, i}
+        {#if i !== menuLinks.length - 1}
+          <li class:flex-basis:100="{menuLinks.length > 8}">
+            <a
+              {href}
+              on:focus
+              on:blur
+              data-event-category="menu"
+              data-event-action="click"
+              data-event-label="{text}"
+              class="text-decoration:none"
+            >
+              {@html text}
+            </a>
+          </li>
+        {:else}
+          <li class:flex-basis:100="{menuLinks.length > 8}">
+            <a
+              {href}
+              on:focus
+              on:blur
+              bind:this="{lastMenuLink}"
+              data-event-category="menu"
+              data-event-action="click"
+              data-event-label="{text}"
+              class="text-decoration:none"
+            >
+              {@html text}
+            </a>
+          </li>
+        {/if}
+      {/each}
+    </ul>
+  </div>
 </nav>
