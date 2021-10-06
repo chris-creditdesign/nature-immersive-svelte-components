db8382c5149cabce8218d3cfa943e16abb2c5acf
Chris Ryan <creditdesign@gmail.com>
2021-10-06 12:10:04 +0100

    Adds VideoButton to index.js

--- src/index.js
+++ src/index.js
@@ -89,6 +89,7 @@ export { default as SimpleButton } from "./components/buttons/SimpleButton/index
 export { default as ToggleButton } from "./components/buttons/ToggleButton/index.svelte";
 export { default as FilterButton } from "./components/buttons/FilterButton/index.svelte";
 export { default as SwitchButton } from "./components/buttons/SwitchButton/index.svelte";
+export { default as VideoButton } from "./components/buttons/VideoButton/index.svelte";
 
 /* --------------------------------- Effects -------------------------------- */
 
